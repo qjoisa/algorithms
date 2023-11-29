de2e44853d9a6c2de9d573821ce878bb71e4c9b6
QJoisa <ujmin04@gmail.com>
2023-11-29 05:15:28 +0500

    Update stack and add a couple of methods

--- dataStructures/stack.go
+++ dataStructures/stack.go
@@ -2,25 +2,31 @@ package dataStructures
 
 import "errors"
 
-type Stack[T any] struct {
+type Stack[T comparable] struct {
 	node *elStack[T]
 }
 
-type elStack[T any] struct {
+type elStack[T comparable] struct {
 	prev *elStack[T]
 	val  T
 }
 
-func (s *Stack[T]) Push(v T) {
-	if s.node == nil {
-		s.node = &elStack[T]{val: v}
-	} else {
-		s.node = &elStack[T]{prev: s.node, val: v}
+func (s *Stack[T]) Push(v ...T) {
+	if len(v) == 0 {
+		return
+	}
+	var i int
+	if s.IsEmpty() {
+		s.node = &elStack[T]{val: v[0]}
+		i = 1
+	}
+	for ; i < len(v); i++ {
+		s.node = &elStack[T]{prev: s.node, val: v[i]}
 	}
 }
 
 func (s *Stack[T]) Pop() (v T, err error) {
-	if s.node == nil {
+	if s.IsEmpty() {
 		return v, errors.New("Stack is empty")
 	} else {
 		v = s.node.val
@@ -29,10 +35,46 @@ func (s *Stack[T]) Pop() (v T, err error) {
 	}
 }
 
+func (s *Stack[T]) Peek() (T, bool) {
+	if !s.IsEmpty() {
+		return s.node.val, true
+	} else {
+		var v T
+		return v, false
+	}
+}
+
+func (s *Stack[T]) Contains(v T) bool {
+	if s.IsEmpty() {
+		return false
+	}
+	for elem := s.node; elem != nil; elem = elem.prev {
+		if elem.val == v {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *Stack[T]) Count() int {
+	if s.IsEmpty() {
+		return 0
+	}
+	var i int
+	for elem := s.node; elem != nil; elem = elem.prev {
+		i++
+	}
+	return i
+}
+
 func (s *Stack[T]) IsEmpty() bool {
 	if s.node == nil {
 		return true
 	} else {
 		return false
 	}
+}
+
+func (s *Stack[T]) Clear() {
+	s.node = nil
 }
